4881bdabbfe8634c95b7dff35078c04345aa2af9
Alex Marquez <alexis.marquez@icloud.com>
2023-02-15 16:57:25 +0100

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { useState } from "react";
-import Loader from './components/Loader';
+import DalleImg from "./images/dalle.jpeg"
+import { Container, Row, Col } from 'react-bootstrap'
 
 const { Configuration, OpenAIApi } = require("openai");
+
 
 const configuration = new Configuration({
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,7 +14,9 @@ const openai = new OpenAIApi(configuration);
 
 
 
+
 function App() {
+
 
   const [userPrompt, setUserPrompt] = useState("")
   const [imageUrl, setImageUrl] = useState("")
@@ -33,35 +37,41 @@ function App() {
   }
 
   return (
+    <div className='home'>
+      <Container>
+        <Row>
+          <Col>
+            <p>Generate a unique image using DALL·E</p>
+            <p>What do you want to see?</p>
+            <input
+              placeholder='A sunset on the Sydney Opera House'
+              onChange={(e) => setUserPrompt(e.target.value)}
+            />
+            <button
+              onClick={() => generateImage()}
+              disabled={loadingImage}>
+              {loadingImage
+                ?
+                'Loading...'
+                :
+                'Generate'}
 
+            </button>
+          </Col>
 
-    <div className="App">
-      {
-        imageUrl
-          ?
-
-          <img src={imageUrl} className="image" alt="ai thing" />
-
-          :
-          <img src="" className="image" alt="ai thing" />
-      }
-      <p>Generate a unique image using DALL·E</p>
-      <p>What do you want to see?</p>
-      <input
-        placeholder='A sunset on the Sydney Opera House'
-        onChange={(e) => setUserPrompt(e.target.value)}
-      />
-      <button
-        onClick={() => generateImage()}
-        disabled={loadingImage}>{loadingImage
-          ?
-          'Loading...'
-          :
-          'Generate'}
-
-      </button>
+          <Col >
+            {imageUrl
+              ?
+              <img src={imageUrl} className="image" alt="ai thing" />
+              :
+              <img src={DalleImg} className="hero" alt="DALL-E" />
+            }
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
+
 }
 
 export default App;
